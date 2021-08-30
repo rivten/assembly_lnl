65bf5cec6fab2686f519a09064b1b11255a449fa
hugo <hugo@gensdeconfiance.com>
2021-08-30 14:44:02 +0200

    fix ex8

--- code/ex8.asm
+++ code/ex8.asm
@@ -1,7 +1,18 @@
 global _start
 
+section .data
+    addr db "yellow"
+
 section .text
 _start:
+    mov [addr], byte 'H'
+    mov [addr + 5], byte '!'
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, addr
+    mov edx, 6
+    int 0x80
+
     mov eax, 1
-    mov ebx, ecx
+    mov ebx, 0
     int 0x80
